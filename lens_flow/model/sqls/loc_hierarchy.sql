c76141c57f92cab92b88a5e1f6ce7f844ba2055d
Nivedhaa Ravichandren <nivedhaa.ravichandren@aciesglobal.com>
2025-01-20 12:40:49 +0530

    updates

---
+++ lens_flow/model/sqls/loc_hierarchy.sql
@@ -0,0 +1,10 @@
+SELECT
+    ds,
+    location_id,
+    region_id,
+    geo_id,
+    hierarchy_level,
+    location_name,
+    region_name,
+    parent_hierarchy_id
+FROM lakehouse.nivedhaa.loc_hierarchy
